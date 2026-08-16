70cc961f62b1ef762fbdba50a4bf1c73560da3fd
Ignacio Marucco <ignaciomarucco@gmail.com>
2026-08-16 19:04:58 -0300

    Add initial project files including HTML, CSS, JS, and assets

---
+++ js-refresher/00-starting-js-proj/assets/scripts/app.js
@@ -0,0 +1,13 @@
+// import { apiKey } from './util.js';
+
+// import apiKey from './util.js';
+
+// import { apiKey, apiSecret } from './util.js';
+
+// import * as util from './util.js';
+// import { apiKey, apiSecret as apiUltraSecret } from './util.js';
+
+// console.log(apiKey);
+// console.log(util.apiKey);
+// console.log(apiUltraSecret);
+
---
+++ js-refresher/00-starting-js-proj/assets/scripts/util.js
@@ -0,0 +1,6 @@
+// export let apiKey = "your-api-key-here";
+
+// export default "your-api-key-here";
+
+// export let apiKey = "your-api-key-here";
+// export let apiSecret = "your-api-secret-here";
