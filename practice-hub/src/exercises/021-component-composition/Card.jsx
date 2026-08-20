4b713afd42e3fad89c8280edbb59a0bbb44764a9
Ignacio <ignaciomarucco@gmail.com>
2026-08-20 17:38:36 -0300

    refactor: update Card component to destructure props and remove TODO comments

--- practice-hub/src/exercises/021-component-composition/Card.jsx
+++ practice-hub/src/exercises/021-component-composition/Card.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-export default function Card(props) {
+export default function Card({ ...props }) {
   // TODO: Build the Card component according to instructions in README.md
   // It should render: <div className="card"><h2>{name}</h2>{children}</div>
   return (
     <div className="card">
-      {/* TODO */}
+      <h2>{props.name}</h2>
+      {props.children}
     </div>
   );
 }
