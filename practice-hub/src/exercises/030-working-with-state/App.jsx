bc489ede251f6444aead74fb297c32db62748665
Ignacio <ignaciomarucco@gmail.com>
2026-08-20 18:12:20 -0300

    feat: implement price update functionality with discount button

--- practice-hub/src/exercises/030-working-with-state/App.jsx
+++ practice-hub/src/exercises/030-working-with-state/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useState } from "react";
 
 // don't change the Component name "App"
 export default function App() {
   // TODO: Add a state value using React.useState and update it from $100 to $75 on button click
+let [price, setPrice] = useState("$100");
+
+function handlePriceUpdate() {
+  setPrice("$75");
+}
 
   return (
     <div>
-      <p data-testid="price">$100</p>
-      <button>Apply Discount</button>
+      <p data-testid="price">{price}</p>
+      <button
+        onClick={() => {
+          handlePriceUpdate();
+        }}
+      >
+        Apply Discount
+      </button>
     </div>
   );
 }
