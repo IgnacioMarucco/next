9e8f480c8ca16c0895e69cdb1283114a3f371d24
Ignacio <ignaciomarucco@gmail.com>
2026-08-20 18:50:42 -0300

    feat: implement toggle functionality for active class in styling

--- practice-hub/src/exercises/036-dynamic-styling/App.jsx
+++ practice-hub/src/exercises/036-dynamic-styling/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from "react";
 
 // don't change the Component name "App"
 export default function App() {
   // TODO: Add state to toggle the 'active' class on click
+  const [clicked, setClicked] = useState(false);
+
+  function handleClick() {
+    setClicked(!clicked);
+  }
 
   return (
     <div>
-      <p>Style me!</p>
-      <button>Toggle style</button>
+      <p className={clicked ? "active" : ""}>Style me!</p>
+      <button onClick={handleClick}>Toggle style</button>
     </div>
   );
 }
