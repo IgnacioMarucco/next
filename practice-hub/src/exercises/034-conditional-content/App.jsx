9919b777a839c671a25250d4c0a902a4a981941a
Ignacio <ignaciomarucco@gmail.com>
2026-08-20 18:34:48 -0300

    feat: implement conditional warning dialog with toggle functionality

--- practice-hub/src/exercises/034-conditional-content/App.jsx
+++ practice-hub/src/exercises/034-conditional-content/App.jsx
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { useState } from "react";
 
 // don't change the Component name "App"
 export default function App() {
   // TODO: Add state to conditionally show / dismiss the warning dialog
+
+  const [clicked, setClicked] = useState(false);
+
+  let boxContent = "";
+
+  // function handleClick() {
+  //   if (clicked === false) {
+  //     setClicked(true);
+  //   } else {
+  //     setClicked(false);
+  //   }
+  // }
+
+  // function handleClick() {
+  //   clicked ? setClicked(false) : setClicked(true);
+  // }
+
+  function handleClick() {
+    setClicked(!clicked);
+  }
+
+  if (!clicked) {
+    boxContent = (
+      <button
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        Delete
+      </button>
+    );
+  } else {
+    boxContent = (
+      <div data-testid="alert" id="alert">
+        <h2>Are you sure?</h2>
+        <p>These changes can't be reverted!</p>
+        <button
+          onClick={() => {
+            handleClick();
+          }}
+        >
+          Proceed
+        </button>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -13,7 +59,15 @@ export default function App() {
         <button>Proceed</button>
       </div>
       */}
-      <button>Delete</button>
+
+      {/* <button
+        onClick={() => {
+          handleClick(clicked);
+        }}
+      >
+        Delete
+      </button> */}
+      {boxContent}
     </div>
   );
 }
